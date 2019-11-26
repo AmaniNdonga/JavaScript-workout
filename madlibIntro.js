422aae0b54be34a04037b09b0d35d314cc397770
Abraham Amani Ndonga <amaniabraham0@gmail.com>
2019-11-26 09:35:14 -0500

    some word blank 'sharpening strings' to create madlibtype

---
+++ madlibIntro.js
@@ -0,0 +1,8 @@
+function wordBlank(myNoun, myAdjective, myVerb, myAdverb) {
+    var result = "";
+    result += "The " + myAdjective + " " + myNoun + " " + myVerb + " to the store " + myAdverb;
+    
+    console.log(result);
+}
+
+wordBlank("man", "big", "ran", "quickly");
